319f4c492bc4440798d2f5f5261e9b35ec81399e
NahumMorales <eliabnahumml@gmail.com>
2024-06-25 16:20:01 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,60 +1,154 @@
-var path, leftBoundary,rightBoundary;
-var pathImg,boyImg;
-var i;
-var boy,boy_running
+var path,boy,cash,diamonds,jwellery,sword;
+var pathImg,boyImg,cashImg,diamondsImg,jwelleryImg,swordImg;
+var treasureCollection = 0;
+var cashG,diamondsG,jwelleryG,swordGroup;
+
+//Game States
+var PLAY=1;
+var END=0;
+var gameState=1;
+
 function preload(){
-  //loadImage de path (camino)
-  pathImg=loadImage("path.png");
-  //loadAnimation de boy (niño)
-  boy_running=loadAnimation("Jake1.png","Jake2.png","jake3.png","jake4.PNG","jake5.png");
- 
+  pathImg = loadImage("Road.png");
+  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
+  cashImg = loadImage("cash.png");
+  diamondsImg = loadImage("diamonds.png");
+  jwelleryImg = loadImage("jwell.png");
+  swordImg = loadImage("sword.png");
+  endImg =loadAnimation("gameOver.png");
 }
 
 function setup(){
   
-  createCanvas(400,400);
- //crear sprite de path (camino) 
-path=createSprite(200,200,100,400);
-//agregar imagen de path
+//crear el canvas y ajustar el tamaño de la ventana para que sea compatible con el dispositivo 
+createCanvas(windowWidth,windowHeight);
+
+path=createSprite(width/2,200);
 path.addImage(pathImg);
-//Hacer que la pista sea un fondo en movimiento al darle velocidad Y.
-path.velocityY=5;
-path.scale=1.2;
+path.velocityY = 4;
 
-//crear sprite de boy (niño)
-boy=createSprite(200,300,20,50);
-//agregar animación para boy
-boy.addAnimation("running",boy_running);
-boy.scale=1;
 
+//crear sprite boy corriendo
+boy = createSprite(width/2,height-20,20,20);
+boy.addAnimation("SahilRunning",boyImg);
+boy.scale=0.08;
   
-// crear  left Boundary (límite izquierdo)
-leftBoundary=createSprite(0,0,100,800);
-////establecer visibilidad como false (falso) para límite izquierdo
-leftBoundary.visible=false;
+  
+cashG=new Group();
+diamondsG=new Group();
+jwelleryG=new Group();
+swordGroup=new Group();
 
-//crear right Boundary (límite derecho)
-rightBoundary=createSprite(410,0,100,800);
-//establecer visibilidad como false (falso) para límite izquierdo
-rightBoundary.visible=false;
 }
 
 function draw() {
+
+  if(gameState===PLAY){
   background(0);
-  path.velocityY = 4;
-  
-  // boy moviéndose en el eje X con el mouse
-  boy.x=mouseX;
+  boy.x = World.mouseX;
   
   edges= createEdgeSprites();
-  boy.collide(edges[3]);
-  // colisión de boy con los límites derecho e izquierdo invisibles 
-  boy.collide(leftBoundary);
-  boy.collide(rightBoundary);
+  boy.collide(edges);
+  
   //código para reiniciar el fondo
-  if(path.y > 400 ){
-    path.y = height/2;
+  if(path.y>height){
+   path.y=height/2 
+  }
+
+    createCash();
+    createDiamonds();
+    createJwellery();
+    createSword();
+
+    if (cashG.isTouching(boy)) {
+      cashG.destroyEach();
+      treasureCollection=treasureCollection + 50;
+    }
+    else if (diamondsG.isTouching(boy)) {
+      diamondsG.destroyEach();
+      treasureCollection=treasureCollection + 100;
+      
+    }else if(jwelleryG.isTouching(boy)) {
+      jwelleryG.destroyEach();
+      treasureCollection= treasureCollection + 150;
+      
+    }else{
+      if(swordGroup.isTouching(boy)) {
+        gameState=END;
+        
+        boy.addAnimation("SahilRunning",endImg);
+        boy.x=width/2;
+        boy.y=height/2;
+        boy.scale=0.6;
+        
+        cashG.destroyEach();
+        diamondsG.destroyEach();
+        jwelleryG.destroyEach();
+        swordGroup.destroyEach();
+        
+        cashG.setVelocityYEach(0);
+        diamondsG.setVelocityYEach(0);
+        jwelleryG.setVelocityYEach(0);
+        swordGroup.setVelocityYEach(0);
+     
+    }
   }
   
   drawSprites();
+  textSize(20);
+  fill(255);
+  text("Tesoro: "+ treasureCollection,width-150,30);
+  }
+
+}
+
+function createCash() {
+  if (World.frameCount % 200 == 0) {
+   //Modificar las posiciones del dinero 
+    var cash = createSprite(Math.round(random(50, 350),40, 10, 10));
+    cash.addImage(cashImg);
+  cash.scale=0.12;
+  cash.velocityY = 5;
+  cash.lifetime = 200;
+  cashG.add(cash);
+  }
+}
+
+function createDiamonds() {
+  if (World.frameCount % 320 == 0) {
+       // Modificar las posiciones de los diamantes 
+
+    var diamonds = createSprite(Math.round(random(50, 350),40, 10, 10));
+    diamonds.addImage(diamondsImg);
+  diamonds.scale=0.03;
+  diamonds.velocityY = 5;
+  diamonds.lifetime = 200;
+  diamondsG.add(diamonds);
+}
+}
+
+function createJwellery() {
+  if (World.frameCount % 410 == 0) {
+    //Modificar las posiciones de las joyas para hacerlas aparecer en el tamaño de pantaña disponible.
+
+    var jwellery = createSprite(Math.round(random(50, 350),40, 10, 10));
+    jwellery.addImage(jwelleryImg);
+  jwellery.scale=0.13;
+  jwellery.velocityY = 5;
+  jwellery.lifetime = 200;
+  jwelleryG.add(jwellery);
+  }
+}
+
+function createSword(){
+  if (World.frameCount % 530 == 0) {
+    //Modificar las prosiciones de la espada para hacerla aparecer en el tamaño de pantaña disponible. 
+
+    var sword = createSprite(Math.round(random(50, 350),40, 10, 10));
+    sword.addImage(swordImg);
+  sword.scale=0.1;
+  sword.velocityY = 4;
+  sword.lifetime = 200;
+  swordGroup.add(sword);
+  }
 }
